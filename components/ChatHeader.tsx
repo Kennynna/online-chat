1ccbca271dbdf1527aba7dd4acadc0e21c999c0d
Kennynna <kennynna@mail.ru>
2024-11-17 20:32:51 +0300

    add components

---
+++ components/ChatHeader.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import { createClient } from "@/utils/supabase/browser";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+// import ChatPresence from "./ChatPresence";
+
+export default function ChatHeader({ user }: { user: User | undefined }) {
+	const router = useRouter();
+
+	const handleLoginWithGithub = () => {
+		const supabase = createClient();
+		supabase.auth.signInWithOAuth({
+			provider: "github",
+			options: {
+				redirectTo: location.origin + "/auth/confirm",
+			},
+		});
+	};
+
+	const handleLogout = async () => {
+		const supabase = createClient();
+		await supabase.auth.signOut();
+		router.refresh();
+	};
+
+	return (
+		<div className="h-20">
+			<div className="p-5 border-b flex items-center justify-between h-full">
+				<div>
+					<h1 className="text-xl font-bold">WYW Chat</h1>
+					{/* <ChatPresence /> */}
+				</div>
+				{user ? (
+					<Button onClick={handleLogout}>Logout</Button>
+				) : (
+					<Button onClick={handleLoginWithGithub}>Login</Button>
+				)}
+			</div>
+		</div>
+	);
+}
